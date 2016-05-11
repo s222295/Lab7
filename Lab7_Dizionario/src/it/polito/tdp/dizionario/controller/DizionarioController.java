aca725ec0f0af48ef3270fc185ca7f3bd3d29999
AssiaP <mariaassunta.palmieri@gmail.com>
2016-05-11 10:55:15 +0200

    Completamento Lab7_Dizionario

--- Lab7_Dizionario/src/it/polito/tdp/dizionario/controller/DizionarioController.java
+++ Lab7_Dizionario/src/it/polito/tdp/dizionario/controller/DizionarioController.java
@@ -1,5 +1,103 @@
 package it.polito.tdp.dizionario.controller;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.*;
+
+import it.polito.tdp.dizionario.Model.Model;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class DizionarioController {
 	
+	private Model model;
+	String testo="";
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private TextField txtNumLettere;
+
+    @FXML
+    private TextField txtParolaDaCercare;
+
+    @FXML
+    private Button btnGeneraGrafo;
+
+    @FXML
+    private Button btnTrovaVicini;
+
+    @FXML
+    private Button btnTrovaConnessi;
+
+    @FXML
+    private TextArea txtArea;
+
+    @FXML
+    private Button btnReset;
+
+    public void setModel(Model model) {
+		this.model=model;
+	}
+    
+    @FXML
+    void doGeneraGrafo(ActionEvent event) {
+    	int n = Integer.parseInt(txtNumLettere.getText()) ;
+    	model.creaGrafo(n); 	
+    }
+
+    @FXML
+    void doReset(ActionEvent event) {
+    	model=new Model();
+    	txtArea.clear();
+    	txtParolaDaCercare.clear();
+    	txtNumLettere.clear();
+    	testo="";
+    }
+
+    
+    
+    @FXML
+    void doTrovaConnessi(ActionEvent event) {
+    	Set<String> vertici = new HashSet<String>();
+    	String parola = txtParolaDaCercare.getText();
+    	vertici.addAll(model.verticiConnessi(parola));
+
+    	testo += "\nTutti i nodi connessi sono: "+vertici.toString();
+    	txtArea.setText(testo);
+    	
+    }
+
+    @FXML
+    void doTrovaVicini(ActionEvent event) {
+
+    	Set<String> vertici = new HashSet<String>();
+    	String parola = txtParolaDaCercare.getText();
+    	vertici.addAll(model.verticiVicini(parola));
+
+    	testo += "\nTutti i nodi vicini sono: "+vertici.toString();
+    	txtArea.setText(testo);
+    }
+
+    @FXML
+    void initialize() {
+        assert txtNumLettere != null : "fx:id=\"txtNumLettere\" was not injected: check your FXML file 'Dizionario.fxml'.";
+        assert txtParolaDaCercare != null : "fx:id=\"txtParolaDaCercare\" was not injected: check your FXML file 'Dizionario.fxml'.";
+        assert btnGeneraGrafo != null : "fx:id=\"btnGeneraGrafo\" was not injected: check your FXML file 'Dizionario.fxml'.";
+        assert btnTrovaVicini != null : "fx:id=\"btnTrovaVicini\" was not injected: check your FXML file 'Dizionario.fxml'.";
+        assert btnTrovaConnessi != null : "fx:id=\"btnTrovaConnessi\" was not injected: check your FXML file 'Dizionario.fxml'.";
+        assert txtArea != null : "fx:id=\"txtArea\" was not injected: check your FXML file 'Dizionario.fxml'.";
+        assert btnReset != null : "fx:id=\"btnReset\" was not injected: check your FXML file 'Dizionario.fxml'.";
+
+    }
+
+	
 }
+
--- Lab7_Dizionario/src/it/polito/tdp/dizionario/controller/Main.java
+++ Lab7_Dizionario/src/it/polito/tdp/dizionario/controller/Main.java
@@ -1,5 +1,6 @@
 package it.polito.tdp.dizionario.controller;
 	
+import it.polito.tdp.dizionario.Model.Model;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
@@ -11,11 +12,16 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Dizionario.fxml"));
-			Scene scene = new Scene(root,400,400);
+			FXMLLoader loader  = new FXMLLoader(getClass().getResource("Dizionario.fxml"));
+			BorderPane root = (BorderPane)loader.load();
+			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
+			
+			Model model = new Model(); 
+			DizionarioController controller = loader.getController();
+			controller.setModel(model);
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
